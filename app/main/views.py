891201c26fa22a37c8294b830574311fc707ac0a
Prince David <dpokeke@gmail.com>
2025-03-31 11:50:40 +0100

    User Authentication System
    User login, register, change-password, change-email, confirm-account, reset-password implementation and test cases for defined methods in user models
    pls run migrations script to implement changes

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from . import main
 @main.route("/")
 def index():
     return render_template('index.html', name="David")
+
+@main.route("/user/<string:username>")
+def user(username):
+    return "<h1>Hello, Welcome To {} Profile</h1>".format(username)
--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,8 @@
 from threading import Thread
-from flask import current_app, render_template
+from functools import wraps
+from flask import current_app, render_template, flash, url_for, redirect
 from flask_mail import Message
+from flask_login import current_user
 from . import mail
 
 
@@ -10,10 +12,20 @@ def send_async_email(app, msg):
 
 
 def send_email(to, subject, template, **kwargs):
-    msg = Message(current_app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=current_app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg = Message(current_app.config['APP_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=current_app.config['APP_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[current_app, msg])
     thr.start()
     return thr
+
+def logout_required(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if current_user.is_authenticated or not current_user.is_anonymous:
+            flash("You are already authenticated.", "info")
+            return redirect(url_for("main.index"))
+        return func(*args, **kwargs)
+
+    return decorated_function
